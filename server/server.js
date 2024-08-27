c214d1eab557f43f576dbc7a5cc94aa58275b8e7
Victoria _Munteanu <munteanuvictoria1@gmail.com>
2024-08-27 19:48:14 +0200

    update

--- server/server.js
+++ server/server.js
@@ -12,3 +12,5 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
+
+//mongodb+srv://Admin_Victoria:<lJwV8fJpjGMSMRg2>@clusterx.govu4la.mongodb.net/?retryWrites=true&w=majority&appName=ClusterX
